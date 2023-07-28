ea03c4922dc1799e525cd8283d3d5472f205229d
caleb765landis <caleb765landis@gmail.com>
2023-07-28 15:27:00 -0600

    Made new repo's first commit the most recent commit

--- Server-Side/Sources/App/configure.swift
+++ Server-Side/Sources/App/configure.swift
@@ -31,3 +31,79 @@ public func configure(_ app: Application) throws {
     // register routes
     try routes(app)
 }
+
+public func checkDataExists(_ app: Application) async throws {
+    if ((try await User.query(on: app.db).count()) == 0) {
+        print("Creating default user..")
+        
+        let tempUser = User(firstName: "Default", lastName: "User", username: "defaultUsername", password: "pass", email: "default@default.com")
+        do {
+            try await tempUser.save(on: app.db)
+            print("User created.")
+        } catch {
+            print("User could not be created.")
+        }
+    } else {
+        print("Loaded default user.")
+    }
+    
+    let userID = try await User.query(on: app.db).first()?.id
+    
+    if ((try await Tag.query(on: app.db).filter(\.$tagName == "Collection").count()) == 0) && userID != nil {
+        print("Creating 'Collection' tag...")
+        
+        let collection = Tag(userID: String(userID!), tagName: "Collection")
+        do {
+            try await collection.save(on: app.db)
+            print("'Collection' tag created.")
+        } catch  {
+            print("'Collection' tag could not be created.")
+        }
+    } else {
+        print("Loaded 'Collection' tag.")
+    } // end if collection exists
+    
+    if ((try await Tag.query(on: app.db).filter(\.$tagName == "Completed").count()) == 0) && userID != nil {
+        print("Creating 'Completed' tag...")
+        
+        let collection = Tag(userID: String(userID!), tagName: "Completed")
+        do {
+            try await collection.save(on: app.db)
+            print("'Completed' tag created.")
+        } catch  {
+            print("'Completed' tag could not be created.")
+        }
+    } else {
+        print("Loaded 'Completed' tag.")
+    } // end if completed exists
+    
+    if ((try await Tag.query(on: app.db).filter(\.$tagName == "Wish List").count()) == 0) && userID != nil {
+        print("Creating 'Wish List' tag...")
+        
+        let collection = Tag(userID: String(userID!), tagName: "Wish List")
+        do {
+            try await collection.save(on: app.db)
+            print("'Wish List' tag created.")
+        } catch  {
+            print("'Wish List' tag could not be created.")
+        }
+    } else {
+        print("Loaded 'Wish List' tag.")
+    } // end if wish list exists
+    
+    if ((try await Tag.query(on: app.db).filter(\.$tagName == "Backlog").count()) == 0) && userID != nil {
+        print("Creating 'Backlog' tag...")
+        
+        let collection = Tag(userID: String(userID!), tagName: "Backlog")
+        do {
+            try await collection.save(on: app.db)
+            print("'Backlog' tag created.")
+        } catch  {
+            print("'Backlog' tag could not be created.")
+        }
+    } else {
+        print("Loaded 'Backlog' tag.")
+    } // end if backlog exists
+    
+}
+
